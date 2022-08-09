65f44d8f027a37e3e3b02e924dc75e8862db1919
Владимир <vladimirramozin@yandex.ru>
2022-08-09 12:49:37 +0300

    тестовые

--- очередь на базе списка.py
+++ очередь на базе списка.py
@@ -4,22 +4,28 @@ import time
 
 
 class Quee():
-    def __init__(self):
+    def __init__(self, max = 0):
         self.quee = []
+        self.max = max
 
     def push(self, value):
-        self.quee.append(value)
+        if len(self.quee)!= self.max:
+            self.quee.append(value)
+        else:
+            return 'error'
 
     def print_quee(self):
         print(self.quee)
 
     def pop(self):
-        if len(self.quee) != 1:
+        if len(self.quee) >= 1:
             for i in range(0, len(self.quee)-1):
                 self.quee[i] = self.quee[i+1]
-            self.quee.pop(i)
-        else:
             self.quee.pop()
+        elif len(self.quee) == 1:
+            self.quee.pop()
+        else:
+            return 'error'
 
 
 def delta_time(start_test, stop_test):
@@ -27,7 +33,7 @@ def delta_time(start_test, stop_test):
 
 
 if __name__ == '__main__':
-    quee = Quee()
+    quee = Quee(10000)
     start_test = time.time()
     for i in range(0, 10000):
         quee.push(i)
